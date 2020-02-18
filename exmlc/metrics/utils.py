53bedb8b815aee62b99b20b0dcb60ea8b2aaec22
Lennart Keller <lennart.keller@googlemail.com>
2020-02-18 15:09:32 +0100

    docs

--- exmlc/metrics/utils.py
+++ exmlc/metrics/utils.py
@@ -6,7 +6,6 @@ def top_n_idx_sparse(matrix: csr_matrix, n: int) -> np.ndarray:
     """
     Return a matrix with of shape (matrix.shape[0], n) with n indices of the n largest value of each row in the matrix
     The indices of the n top values are not sorted itself.
-    NOTE: If there are less than n values
     :param matrix:
     :param n:
     :return:
